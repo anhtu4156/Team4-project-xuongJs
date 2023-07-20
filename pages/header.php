46ef82b9dc79ba1b1999257a0770d638429b2eb4
anhtu4156 <anhtu4156@gmail.com>
2023-07-20 14:34:27 +0700

    update file

---
+++ pages/header.php
@@ -0,0 +1,30 @@
+<div class="box-header">
+                <div class="left">
+                    
+                        <ul>
+                            <li>
+                                <a href="#"><i class="fa-solid fa-plane"></i> Find Flight</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa-solid fa-bed"></i> Find Stays</a>
+                            </li>
+                        </ul>
+                    
+
+                    <img src="../img/Logo.svg" alt="logo">
+                </div>
+                <div class="right">
+                    <li>
+                        <a href="#"><i class="fa-solid fa-heart"></i> Favourites</a>
+                    </li>
+                    <span>|</span>
+                    <div class="account">
+                        <a href="#">
+                            <div class="box-img">
+                                <img src="../img/anh.png" alt="">
+                            </div>
+                        </a>
+                        <a href="">Account</a>
+                    </div>
+                </div>
+            </div>
