bbe80a90eb4d893dc70bd4fe3ae33190d1af9417
Minh Le <121961255+minhle3107@users.noreply.github.com>
2023-07-25 01:53:16 +0700

    Delete project-newstech directory

--- project-newstech/src/utilities/index.js
+++
@@ -1,6 +0,0 @@
-import Navigo from "navigo";
-const router = new Navigo("/", { linksSelector: "a", hash: true });
-const render = (content, target) => {
-  target.innerHTML = content();
-};
-export { render, router };
